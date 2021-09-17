eee309746683bda178bf80cd464cca8834dd1f34
ko-ing <318921@naver.com>
2021-09-17 23:09:46 +0900

    변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ko-ing's blog
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ko-ing"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          COMING SOON
         </a>
       </header>
     </div>

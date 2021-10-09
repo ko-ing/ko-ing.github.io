449c870723b125dad0ecf6abe31e7c25d2f9740e
ko-ing <318921@naver.com>
2021-10-09 21:48:46 +0900

    md 읽어서 블로그 글 올리기

---
+++ public/writings/whydontyouknow.md
@@ -0,0 +1,14 @@
+# A preview of `ko-ing blog`
+
+This blog will be about anything from my head.
+
+👉 Workgin as a SErVer deVEloPEr now at VCNC in Seoul, Korea.
+
+## Overview: What Will I Post?
+
+* Diaries: Anything,, working, living, exercising, relationships,,, etc.
+* Development related things: ['my-notion'](https://grizzled-saxophone-561.notion.site/15d76597f72648f8bcb035344f92d9ab)
+* I DUNNO,,,
+* Just For Fun
+
+## COMMING SOOOOON
--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.jpg';
 import './App.css';
 import Header from './header/Header';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
+import { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+
 
 const WholeWrapper = styled.div`
   min-width: 320px;
@@ -31,23 +34,34 @@ const MainWrapper = styled.div`
 `
 
 const ContentWrapper = styled.div`
-
+  width: 100%;
 `
 
+
+const markdownFiles = require.context('./writings', false, /\.md$/)
+  .keys()
+  .map(a => "./writings" + a.substring(1))
+  
+
 function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    Promise.all(markdownFiles.map((file) => fetch(file)
+      .then((res) => res.text())))
+      .then((posts) => setPosts(posts))
+      .catch((err) => console.error(err));
+  }, [])
+
   return (
     <WholeWrapper>
       <MainWrapper>
         <Header />
         <ContentWrapper>
-          {[...Array(10000)].map((e, i) => (
+          {posts.map((post) => (
             <>
-              <br/>
-              COMMING SOON
-              <br/>
-              TO THE NEXT LEVEL
-            </>
-          ))}
+              <ReactMarkdown children={post} />
+            </>)
+          )}
         </ContentWrapper>
       </MainWrapper>
     </WholeWrapper>
